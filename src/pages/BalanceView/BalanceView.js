e5912f6adc1fe3e8d35afbdeba7d5418dc1b380f
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-11-07 03:35:24 +0200

    add styles Balance

--- src/pages/BalanceView/BalanceView.js
+++ src/pages/BalanceView/BalanceView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'components/Container';
+import Balance from "components/Balance";
 import OnLoader from 'components/OnLoader';
 
 const BalanceView = () => {
   return (
     <Container>
+      <Balance />
       <OnLoader />
     </Container>
   );
