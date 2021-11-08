98d2b95a1c301cc18060dba4433ee154ca18e2a1
virysik <68701496+virysik@users.noreply.github.com>
2021-11-08 19:46:28 +0100

    fix container

--- src/pages/ReportsView/ReportsView.js
+++ src/pages/ReportsView/ReportsView.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Container } from 'components/Container';
-
+import Container from 'components/Container';
 import s from './ReportView.module.css';
 
 const ReportsView = () => {
   return (
-    <Container>
-      <div className={s.containerReport}></div>
-    </Container>
+    <section className={s.section}>
+      <Container></Container>
+    </section>
   );
 };
 
