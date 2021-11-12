5c3a2ec216b6dcf1ca19bf8617e806a793709426
virysik <68701496+virysik@users.noreply.github.com>
2021-11-12 12:01:17 +0100

    fixed report part

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,36 +1,17 @@
-import React from 'react';
+import { CurrentMonth } from './CurrentMonth';
 import s from './Report.module.css';
 
-const Report = ({ items }) => {
+const Report = ({ month, year, onHandleClickRight, onHandleClickLeft }) => {
   return (
-    <table className={s.reportHistory}>
-      <tr>
-        <th className={s.reportHeader} colspan="3">
-          Сводка
-        </th>
-      </tr>
-      {items.map(x => (
-        <tr key={x.id}>
-          <td className={s.reportData}>{x.month}</td>
-          <td className={s.reportData}>{x.sum}</td>
-        </tr>
-      ))}
-    </table>
+    <div className={s.navigationWrapper}>
+      <CurrentMonth
+        currentMonth={month}
+        currentYear={year}
+        onHandleClickRight={onHandleClickRight}
+        onHandleClickLeft={onHandleClickLeft}
+      />
+    </div>
   );
 };
-// import { CurrentMonth } from './CurrentMonth';
-
-// const Report = ({ month, year, onHandleClickRight, onHandleClickLeft }) => {
-//   return (
-//     <div className={s.navigationWrapper}>
-//       <CurrentMonth
-//         currentMonth={month}
-//         currentYear={year}
-//         onHandleClickRight={onHandleClickRight}
-//         onHandleClickLeft={onHandleClickLeft}
-//       />
-//     </div>
-//   );
-// };
 
 export default Report;
