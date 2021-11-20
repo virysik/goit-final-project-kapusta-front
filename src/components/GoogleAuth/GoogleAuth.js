44481a4cf30f53ba4adf75963ffc7320c3f23f8b
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-11-20 19:35:13 +0200

    googleRegistartion and googleLogIn

--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -1,32 +1,44 @@
-import GoogleLogin from 'react-google-login';
-import GoogleAuthButton from './GoogleAuthButton';
-import { useDispatch } from 'react-redux';
-import { googleLogIn } from '../../redux/auth/auth-slice';
+// import GoogleLogin from 'react-google-login';
+// import GoogleAuthButton from './GoogleAuthButton';
+import { ReactComponent as GoogleIcon } from '../../images/svg/google.svg';
+import s from './GoogleAuth.module.css';
+// import { useDispatch } from 'react-redux';
+// import { googleLogIn } from '../../redux/auth/auth-slice';
+
+// const BASE_URL = 'https://kapusta-team-project.herokuapp.com/api';
+const BASE_URL = 'http://localhost:3030/api';
 
 const GoogleAuth = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const successLogin = async ({ tokenId, profileObj }) => {
-    const { email, name, balance } = profileObj;
-    const newUser = {
-      email,
-      name,
-      balance,
-      tokenId,
-    };
+  // const successLogin = async ({ tokenId, profileObj }) => {
+  //   const { email, name, balance } = profileObj;
+  //   const newUser = {
+  //     email,
+  //     name,
+  //     balance,
+  //     tokenId,
+  //   };
 
-    dispatch(googleLogIn(newUser));
-  };
+  // dispatch(googleLogIn(newUser));
+  // };
   return (
     <>
-      <GoogleLogin
-        clientId="247340085219-f8vpeflsvkcin8lei0r36j2eu8gqj2uh.apps.googleusercontent.com"
+      <a href={`${BASE_URL}/users/google`}>
+        <button type="button" className={s.buttonGoogle}>
+          <GoogleIcon className={s.googleSvg} />
+          Google
+        </button>
+      </a>
+      {/* <GoogleLogin
+        // clientId="247340085219-f8vpeflsvkcin8lei0r36j2eu8gqj2uh.apps.googleusercontent.com"
+        clientId="208854685975-urv5h1d3c44rt5o3esntuec8m5rrrovc.apps.googleusercontent.com"
         render={({ onClick, disabled }) => (
           <GoogleAuthButton onClick={onClick} disabled={disabled} />
         )}
         onSuccess={successLogin}
         cookiePolicy={'single_host_origin'}
-      />
+      /> */}
     </>
   );
 };
