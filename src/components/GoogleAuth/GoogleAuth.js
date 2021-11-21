3e2dbbafafe62a94159c99f5ecb02fe5de085287
Vera Kuzjo <68701496+virysik@users.noreply.github.com>
2021-11-21 13:12:52 +0100

    Update GoogleAuth.js

--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -1,8 +1,8 @@
 import { ReactComponent as GoogleIcon } from '../../images/svg/google.svg';
 import s from './GoogleAuth.module.css';
 
-// const BASE_URL = 'https://kapusta-team-project.herokuapp.com/api';
-const BASE_URL = 'http://localhost:3030/api';
+const BASE_URL = 'https://kapusta-team-project.herokuapp.com/api';
+// const BASE_URL = 'http://localhost:3030/api';
 
 const GoogleAuth = () => {
   return (
